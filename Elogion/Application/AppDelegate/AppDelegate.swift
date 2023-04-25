7cf4922a003b81226ea2ea20819b5e46a7b449b9
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-25 06:22:58 +0300

    AppRouter born

--- Elogion/Application/AppDelegate/AppDelegate.swift
+++ Elogion/Application/AppDelegate/AppDelegate.swift
@@ -9,10 +9,17 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private var router: AppRoutable?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        router = AppRouter()
         
-        print(#function)
+        routeToInitialScene()
         
         return true
+    }
+    
+    private func routeToInitialScene() {
+        self.router?.routeToInitialScene()
     }
 }
