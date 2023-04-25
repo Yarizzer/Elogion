e7691964000101ab4650f97c8d0c26069b6b0e84
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-25 06:19:24 +0300

    Prepared blank project

---
+++ Elogion/Application/AppDelegate/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Elogion
+//
+//  Created by Yaroslav Abaturov on 25.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print(#function)
+        
+        return true
+    }
+}
