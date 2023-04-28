e0c1cc55b25243e4923595ebc75cc1f1b1309a50
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-28 05:48:30 +0300

    Publisher

--- Elogion/Application/AppCore/Layers/AILayer/AppAILayer.swift
+++ Elogion/Application/AppCore/Layers/AILayer/AppAILayer.swift
@@ -5,14 +5,29 @@
 //  Created by Yaroslav Abaturov on 25.04.2023.
 //
 
+import CoreML
+
 class AppAILayer {
     init() {
         self.objectPredictor = ObjectPredictor()
     }
+    //Publishers
+    var data: Publisher<String?> = Publisher(nil)
     
     private let objectPredictor: ObjectPredictable
 }
 
 extension AppAILayer: AppAILayerType {
-    
+    //Functions
+    func updatePrediction(for image: CVPixelBuffer) {
+//        guard let mlModel else { return }
+//        
+//        let newPrediction = try? mlModel.prediction(image: image).classLabel
+//        
+//        guard data.value != newPrediction else { return }
+//        
+//        DispatchQueue.main.async { [weak self] in
+//            self?.data.value = newPrediction
+//        }
+    }
 }
--- Elogion/Application/AppCore/Layers/AILayer/AppAILayerType.swift
+++ Elogion/Application/AppCore/Layers/AILayer/AppAILayerType.swift
@@ -5,6 +5,12 @@
 //  Created by Yaroslav Abaturov on 25.04.2023.
 //
 
+import CoreML
+
 protocol AppAILayerType {
+    //Publishers
+    var data: Publisher<String?> { get }
     
+    //Functions
+    func updatePrediction(for image: CVPixelBuffer)
 }
